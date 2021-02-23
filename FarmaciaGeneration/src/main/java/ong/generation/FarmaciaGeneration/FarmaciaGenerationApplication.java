f7826206b4955f3e1083793e3e7eb0621615b146
LPaifar <lucaspaifar1996@gmail.com>
2021-02-23 11:28:52 -0300

    Exercicio proposto - Farmacia_Generation

---
+++ FarmaciaGeneration/src/main/java/ong/generation/FarmaciaGeneration/FarmaciaGenerationApplication.java
@@ -0,0 +1,13 @@
+package ong.generation.FarmaciaGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaGenerationApplication.class, args);
+	}
+
+}
---
+++ FarmaciaGeneration/src/test/java/ong/generation/FarmaciaGeneration/FarmaciaGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package ong.generation.FarmaciaGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
